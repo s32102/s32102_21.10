c3e7ca20da01b4be89803dbca1aaea70bca75a2a
s32102 <s32102@pjwstk.edu.pl>
2025-10-26 22:41:04 +0100

    sprint, jump and character flip

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -7,9 +7,16 @@ public class PlayerControler11c : MonoBehaviour
 {
     // Start is called before the first frame update
     public float moveSpeed = 1000;
+    public float sprintmultiplier = 1.5f;           
     public float jumpForce = 300;
+
+    public Transform groundCheck;
+    public float groundCheckRadius  = 0.2f;
+    public LayerMask whatIsGround;
+
     public Rigidbody2D rigidbody2;
     public SpriteRenderer spriteRenderer;
+    private bool isGrounded;
 
     void Start()
     {
@@ -21,14 +28,36 @@ public class PlayerControler11c : MonoBehaviour
     void Update()
     {
         float moveInput = Input.GetAxis("Horizontal");
-        rigidbody2.linearVelocity = new Vector2(moveInput *
-            moveSpeed *
-            Time.deltaTime,
-            rigidbody2.linearVelocity.y);
-        if (Input.GetKeyDown(KeyCode.Space))
+        rigidbody2.linearVelocity = new Vector2(moveInput * moveSpeed * Time.deltaTime, rigidbody2.linearVelocity.y);
+       
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+
+        if (isGrounded && Input.GetKeyDown(KeyCode.Space))
         {
             rigidbody2.AddForce(Vector2.up * jumpForce);
         }
     }
+
+    private void FixedUpdate()
+    {
+        float moveInput = Input.GetAxis("Horizontal");
+        if (moveInput > 0)
+        {
+            spriteRenderer.flipX = false;
+        }
+        else if (moveInput < 0)
+        {
+            spriteRenderer.flipX = true;
+        }
+
+        float currentMoveSpeed = moveSpeed;
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            currentMoveSpeed *= sprintmultiplier;
+        }
+
+        rigidbody2.linearVelocity = new Vector2(moveInput * currentMoveSpeed * Time.fixedDeltaTime, rigidbody2.linearVelocity.y);
+    }
 }
+
 
